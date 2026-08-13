3a7f4211c71f2ca79997c3475bb9f5072f0dfe6f
snehalXcode <snehalsathawane2511@gmail.com>
2026-08-13 21:09:12 +0530

    Loops Question

---
+++ loopques.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+public class loopques {
+    public static void main(String[] args) {
+        // print 1 to n
+        Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // for (int i = 1; i <= n; i++) {
+        //     System.out.println(i);
+        //  }
+
+        // even numbers from 1 to n
+        // for (int i = 1; i <= n; i++) {
+        //     if (i % 2 == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+
+        // table of n
+        // for(int i = 1; i <= 10; i++) {
+        //     System.out.println(n*i);
+        // }
+
+        // print odd number divisible by 3
+        // for (int i = 1; i <= n; i++) {
+        //     if (i % 2 != 0 && i % 3 == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+
+        // arithmetic progression
+        // for(int i = 1; i <= n; i++) {
+        //     System.out.println(3*i + 2);
+        // }
+
+        // digits of number
+        //int count = 0;
+        // while (n > 0) {
+        //     n = n / 10;
+        //     count++;
+        // }
+        // System.out.println("Number of digits: " + count);
+
+        // sum of natural numbers
+        // int sum=0;
+        // for(int i = 1; i <= n; i++) {
+        //     sum = sum + i;
+        // }
+        // System.out.println("Sum of digits: " + sum);
+
+        // sum of digits of number
+        // int sum=0;
+        // while (n > 0) {
+        //     int ld= n % 10;
+        //     sum = sum + ld;
+        //     n = n / 10;
+        // }
+        // System.out.println("Sum of digits: " + sum);
+    
+        // Reverse of number
+        // int rev=0;
+        // while (n > 0) {
+        //     int ld= n % 10;
+        //     rev = rev * 10 + ld;
+        //     n = n / 10;
+        // }
+        // System.out.println("Reverse of number: " + rev);
+
+        // sum of number and its reverse
+        // int rev=0;
+        // int sum=0;
+        // while (n > 0) {
+        //     int ld= n % 10;
+        //     rev = rev * 10 + ld;
+        //     sum = sum + ld;
+        //     n = n / 10;
+        // }
+        // System.out.println("Reverse of number: " + rev);
+        // System.out.println("Sum of digits: " + sum);
+
+        // factorial of number
+        // int fact=1;
+        // for(int i = 1; i <= n; i++) {
+        //     fact = fact * i;
+        // }
+        // System.out.println("Factorial of number: " + fact);
+
+        // print n to 1 number
+        // for(int i = n; i >= 1; i--) {
+        //     System.out.println(i);
+        // }
+
+        // a raised to the power b
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int res=1;
+        // for(int i = 1; i <= b; i++) {
+        //     res = res * a;
+        // }
+        // System.out.println("Result: " + res);
+    }
+    
+}
